8d28198451390c84a4e7a371b643d1d198054251
Logan Wu <yik2@pdx.edu>
2015-10-07 17:19:33 -0700

    make night mode work

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -2,8 +2,8 @@ MovieReviews.directive("nightMode", function() {
   return {
       restrict:"A",
       link: function(scope,element,attrs) {
-          element.bind('cilck', function() {
-             angular.element(document.querySelector('#body-element')).toggleClass('nightmode'); 
+          element.bind('click', function() {
+             angular.element(document.querySelector('#body-element')).toggleClass('nightmode');
           });
       }
   }
